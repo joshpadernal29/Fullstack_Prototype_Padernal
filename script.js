2d02189f5cc7c5a4c8ec60582b202c2e998a2293
joshpadernal29 <josh.padernal29@gmail.com>
2026-02-13 01:35:00 +0800

    added account registration function

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let currentUser = null;
 
 function navigateTo(hash) {
-    window.location.hash;
+    window.location.hash = hash;
 }
 
 function handleRouting() {
@@ -17,13 +17,16 @@ function handleRouting() {
 
     // switch between section id
     let sectionId;
-    if (hash === '#/') {
+    if (hash === '#/' || hash === '') {
         sectionId = "home-page";
     } else if (hash === '#/register') {
         sectionId = "register-page";
     } else if (hash === '#/login') {
         sectionId = 'login-page';
+    } else if (hash === '#/verify') {
+        sectionId = 'verify-email';
     }
+
 
     // show page
     const activePage = document.getElementById(sectionId);
@@ -31,6 +34,57 @@ function handleRouting() {
         activePage.style.display = 'block';
     }
 }
+// set to default home page
+window.addEventListener('load', () => {
+    // If the URL is empty, set it to home page
+    if (!window.location.hash || window.location.hash === "#") {
+        window.location.replace("#/");
+    }
 
+    handleRouting();
+});
 window.addEventListener('hashchange', handleRouting);
+
+// Authentication
+// temp db
+window.db = {
+    accounts: []
+};
+
+function registration(event) {
+    // check if email already exist in window.db.accounts
+    const emailExist = window.db.accounts.some(acc => acc.email === email);
+    event.preventDefault();
+
+    // get input data
+    const inputFname = document.getElementById('fname').value;
+    const inputLname = document.getElementById('lname').value;
+    const inputEmail = document.getElementById('email').value;
+    const inputPassword = document.getElementById('password').value;
+
+    // check if email already exists
+    if (emailExist) {
+        alert("Email already Exists!");
+    } else {
+        // save new account 
+        const newAccount = {
+            Fname: inputFname,
+            Lname: inputLname,
+            email: inputEmail,
+            password: inputPassword,
+            verified: false
+        };
+
+        console.log("account pushed:" + inputEmail);
+        window.db.accounts.push(newAccount);
+
+        //save to local storage
+        localStorage.setItem('accounts', JSON.stringify(window.db.accounts));
+        localStorage.setItem('unverified_email', inputEmail);
+
+        let userEmail = document.getElementById('email').value;
+        document.getElementById('showEmail').innerText = inputEmail;
+        navigateTo('#/verify');
+    }
+}
 
